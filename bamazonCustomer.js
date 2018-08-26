72f65642dd8d0f7975ce8808c150cfd840e0bcdc
mv04 <mtvickery1@gmail.com>
2018-08-26 18:33:34 -0500

    working customer interface

---
+++ bamazonCustomer.js
@@ -0,0 +1,108 @@
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+var chalk = require("chalk");
+
+var connection = mysql.createConnection({
+    host: "127.0.0.1",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected as id " + connection.threadId);
+    displayTable();
+});
+
+var displayTable = function () {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log(
+                chalk.red(res[i].item_id) + " - ",
+                chalk.green("Product: ") + res[i].product_name,
+                chalk.green("Department: ") + res[i].department_name,
+                chalk.green("Price: ") + res[i].price,
+                chalk.green("Available: ") + res[i].stock_quantity);
+        }
+        startShopping(res);
+    })
+}
+
+var startShopping = function (res) {
+    inquirer.prompt([
+        {
+            name: "productId",
+            type: "input",
+            message: "What is the ID of the product that you would like to purchase?",
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "How many would you like?"
+        }
+    ])
+        .then(function (answer) {
+            var chosenProduct;
+            var input;
+            for (var i = 0; i < res.length; i++) {
+                if (parseInt(answer.productId) === res[i].item_id && res[i].stock_quantity <= 0) {
+                    console.log("Out of stock! Please make another selection.");
+                    startShopping(res);
+                } else if (parseInt(answer.productId) === res[i].item_id) {
+                    chosenProduct = res[i];
+                    input = answer;
+                    fulfillOrder(chosenProduct, input);
+                }
+            }
+        })
+}
+
+var fulfillOrder = function (chosenProduct, input) {
+    var newQuantity = chosenProduct.stock_quantity - input.quantity;
+    connection.query("UPDATE products SET ? WHERE ?",
+        [
+            {
+                stock_quantity: newQuantity
+            },
+            {
+                item_id: input.productId
+            }
+        ],
+        function (err) {
+            if (err) throw err;
+            connection.query("SELECT * FROM products WHERE item_id=?",
+                [
+                    input.productId
+                ],
+                function (err, res) {
+                    if (err) throw err;
+                    var total = input.quantity * res[0].price;
+                    var quantityPurchased = input.quantity;
+                    var itemPurchased = res[0].product_name;
+                    console.log("You purchased " + quantityPurchased + " " + itemPurchased + "(s) for a total of $" + total + ".");
+                    wannaBuyMore();
+                }
+            )
+        }
+    )
+}
+
+var wannaBuyMore = function () {
+    inquirer.prompt([
+        {
+        name: "buyMore",
+        type: "confirm",
+        message: "Would you like to purchase anything else?",
+        }
+    ]).then(function (input) {
+        if (input.buyMore) {
+            displayTable()
+        } else {
+            console.log("BYE!");
+            connection.end();
+        }
+    })
+}
---
+++ schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products(
+  item_id INT AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(50),
+  department_name VARCHAR(50),
+  price INT,
+  stock_quantity INT,
+  PRIMARY KEY (item_id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES 
+("Drone", "Electronics", 1200, 50),
+("Bag of Candy", "Food", 1, 50),
+("Kayak", "Outdoor", 500, 20),
+("Bike", "Outdoor", 300, 100),
+("Shrek on VHS", "Entertainment", 5, 1000),
+("Pack of Bacon", "Food", 7, 80),
+("Tank Tops", "Clothing", 10, 500),
+("Biscuits and Gravy", "Food", 25, 100),
+("Pumpkin", "Seasonal", 10, 200),
+("Palm Tree", "Garden", 100, 25);
+
+
+SELECT * FROM products;
